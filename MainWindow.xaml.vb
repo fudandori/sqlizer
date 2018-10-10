9de886eeafbf4f7f23fe7c33e67c3b97272f353e
Fermin Hijar <fudandori@gmail.com>
2018-10-10 02:17:22 +0200

    Added window theme, keyword recoloring

--- MainWindow.xaml.vb
+++ MainWindow.xaml.vb
@@ -35,6 +35,7 @@ Class MainWindow
         'Write the result
         ToTextBox.AppendText(processedText)
 
+        Recolor()
     End Sub
 
     ''' <summary>
@@ -54,25 +55,28 @@ Class MainWindow
         'Removal of carriage returns
         processedText = processedText.Replace(vbCrLf, "")
 
-        'Trim everything before first double quotes, if there is anything
+        'Remove everything before first double quotes, if there is anything
         Dim firstQuotes As Integer = processedText.IndexOf("""") + 1
         processedText = If(firstQuotes > -1, processedText.Remove(0, firstQuotes), processedText)
 
-        'Trim everything after last double quotes, if there's anything
+        'Remove everything after last double quotes, if there's anything
         Dim lastQuotes As Integer = processedText.LastIndexOf("""")
         processedText = If(lastQuotes > -1, processedText.Remove(lastQuotes), processedText)
 
-        'Regex pattern declaration
-        Dim concatRegex As String = """[\+\s]*"""
-
         'Removal of JAVA concatenation syntax
-        processedText = Regex.Replace(processedText, concatRegex, "")
+        processedText = Regex.Replace(processedText, """[\+\s]*""", "")
 
-        'Removal of carriage returns
+        'Removal of JAVA carriage returns
         processedText = Regex.Replace(processedText, "\\r\\n", "")
 
+        'Removal of spaces with line break
         processedText = Regex.Replace(processedText, "\s+\n", "")
+
+        'Trim stacks of spaces into a single space
         processedText = Regex.Replace(processedText, "\s+", " ")
+
+        'Removal of a space at the begining, if there is any
+        processedText = Regex.Replace(processedText, "^\s", "")
 
     End Sub
 
@@ -81,5 +85,34 @@ Class MainWindow
 
         FromTextBox.FontSize = defaultFontSize * slider.Value
         ToTextBox.FontSize = defaultFontSize * slider.Value
+    End Sub
+
+    Private Sub Recolor()
+        ToTextBox.SelectAll()
+        Dim selection As String = ToTextBox.Selection.Text & " "
+        ToTextBox.Selection.Text = ""
+
+        Dim buffer As String = ""
+
+        For Each c As Char In selection
+            If (c = " "c) Then
+                If Regex.IsMatch(buffer, "SELECT|FROM|WHERE") Then
+                    DyeWord(buffer & c, Brushes.Blue, True)
+                Else
+                    DyeWord(buffer & c, Brushes.Black, False)
+                End If
+                buffer = ""
+            Else
+                buffer &= c
+            End If
+        Next
+
+    End Sub
+
+    Private Sub DyeWord(word As String, brush As SolidColorBrush, bold As Boolean)
+        Dim range As TextRange = New TextRange(ToTextBox.Document.ContentEnd, ToTextBox.Document.ContentEnd)
+        range.Text = word
+        range.ApplyPropertyValue(TextElement.ForegroundProperty, brush)
+        range.ApplyPropertyValue(TextElement.FontWeightProperty, If(bold, FontWeights.Bold, FontWeights.Regular))
     End Sub
 End Class
